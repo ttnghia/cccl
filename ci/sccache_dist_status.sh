2ca46f7489c91b6048c5095b643aee0924e29cc8
ptaylor <paul.e.taylor@me.com>
2025-01-14 16:26:56 -0800

    include timestamp in dist stats

--- ci/sccache_dist_status.sh
+++ ci/sccache_dist_status.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 # Print current dist status to verify we're connected
 sccache --dist-status | jq -r -f <(cat <<"EOF"
   .SchedulerStatus as [$x, $y] | [
-    ($y | {type: "scheduler", id: $x, servers: (.servers | length), cpus: .info.occupancy, util: ((.info.cpu_usage // 0) * 100 | round | . / 100 | tostring | . + "%"), jobs: (.jobs.fetched + .jobs.running), fetched: .jobs.fetched, running: .jobs.running, u_time: ($y.servers // {} | map(.u_time) | min | . // 0 | tostring | . + "s")}),
+    ($y | {time: now | floor, type: "scheduler", id: $x, servers: (.servers | length), cpus: .info.occupancy, util: ((.info.cpu_usage // 0) * 100 | round | . / 100 | tostring | . + "%"), jobs: (.jobs.fetched + .jobs.running), fetched: .jobs.fetched, running: .jobs.running, u_time: ($y.servers // {} | map(.u_time) | min | . // 0 | tostring | . + "s")}),
     ($y.servers // {}
       | to_entries
       | sort_by(.key)
       | map(
         .key as $k
         | .value
-        | {type: "server", id: .id, servers: 1, cpus: .info.occupancy, util: ((.info.cpu_usage // 0) * 100 | round | . / 100 | tostring | . + "%"), jobs: (.jobs.fetched + .jobs.running), fetched: .jobs.fetched, running: .jobs.running, u_time: ((.u_time // 0) | tostring | . + "s")}
+        | {time: now | floor, type: "server", id: .id, servers: 1, cpus: .info.occupancy, util: ((.info.cpu_usage // 0) * 100 | round | . / 100 | tostring | . + "%"), jobs: (.jobs.fetched + .jobs.running), fetched: .jobs.fetched, running: .jobs.running, u_time: ((.u_time // 0) | tostring | . + "s")}
       )[]
     )
   ] as $rows
