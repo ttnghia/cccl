dd3d91fe4c6f06a8af729d1ef58c91cb7e5c0b3b
ptaylor <paul.e.taylor@me.com>
2025-01-24 13:17:12 -0800

    bump sccache version [skip-vdc] [skip-docs] [skip-rapids]

--- ci/sccache_dist_status.sh
+++ ci/sccache_dist_status.sh
@@ -3,19 +3,26 @@
 set -euo pipefail
 
 # Print current dist status to verify we're connected
+# Print current dist status to verify we're connected
 sccache --dist-status | jq -r -f <(cat <<"EOF"
+  def info_to_row: {
+    time: now | floor,
+    type: (.type // "server"),
+    id: .id,
+    servers: ((.servers | length) // 1),
+    cpus: .info.occupancy,
+    util: ((.info.cpu_usage // 0) * 100 | round | . / 100 | tostring | . + "%"),
+    jobs: (.jobs.fetched + .jobs.running),
+    fetched: .jobs.fetched,
+    running: .jobs.running,
+    u_time: ((.u_time // 0) | tostring | . + "s")
+  };
+
   .SchedulerStatus as [$x, $y] | [
-    ($y | {time: now | floor, type: "scheduler", id: $x, servers: (.servers | length), cpus: .info.occupancy, util: ((.info.cpu_usage // 0) * 100 | round | . / 100 | tostring | . + "%"), jobs: (.jobs.fetched + .jobs.running), fetched: .jobs.fetched, running: .jobs.running, u_time: ($y.servers // {} | map(.u_time) | min | . // 0 | tostring | . + "s")}),
-    ($y.servers // {}
-      | to_entries
-      | sort_by(.key)
-      | map(
-        .key as $k
-        | .value
-        | {time: now | floor, type: "server", id: .id, servers: 1, cpus: .info.occupancy, util: ((.info.cpu_usage // 0) * 100 | round | . / 100 | tostring | . + "%"), jobs: (.jobs.fetched + .jobs.running), fetched: .jobs.fetched, running: .jobs.running, u_time: ((.u_time // 0) | tostring | . + "s")}
-      )[]
-    )
-  ] as $rows
+    ($y + { id: $x, type: "scheduler", u_time: ($y.servers // {} | map(.u_time) | min | . // 0 | tostring) }),
+    ($y.servers // [] | sort_by(.id)[])
+  ]
+  | map(info_to_row) as $rows
   | ($rows[0] | keys_unsorted) as $cols
   | ($rows | map(. as $row | $cols | map($row[.]))) as $rows
   | $cols, $rows[] | @csv
