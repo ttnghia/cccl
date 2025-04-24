f7b7736ca2e959c60f17117ea224510bf4aa6b8e
fbusato <fbusato@nvidia.com>
2025-04-24 16:21:09 -0700

    try to remove zero_value

--- libcudacxx/test/libcudacxx/std/numerics/numeric.ops/numeric.ops.sat/add_sat.pass.cpp
+++ libcudacxx/test/libcudacxx/std/numerics/numeric.ops/numeric.ops.sat/add_sat.pass.cpp
@@ -19,14 +19,11 @@
 #include <cuda/std/type_traits>
 
 template <class I>
-__host__ __device__ constexpr void test_add_sat(I x, I y, I res, int zero_value)
+__host__ __device__ constexpr void test_add_sat(I x, I y, I res)
 {
-  if constexpr (cuda::std::is_same_v<I, uint16_t>)
-  {
-    auto ret = cuda::std::add_sat(static_cast<I>(zero_value + x), static_cast<I>(zero_value + y));
-    printf("TEST: %d   %d  (%d)\n", (int) ret, (int) res, zero_value);
-  }
-  assert(cuda::std::add_sat(static_cast<I>(zero_value + x), static_cast<I>(zero_value + y)) == res);
+  auto retval = cuda::std::add_sat(x, y);
+  printf("TEST: %d   %d\n", (int) retval, (int) res);
+  assert(retval == res);
 }
 
 template <class I>
@@ -95,7 +92,7 @@ __host__ __device__ constexpr void test_signed(int zero_value)
 }
 
 template <class I>
-__host__ __device__ constexpr void test_unsigned(int zero_value)
+__host__ __device__ constexpr void test_unsigned()
 {
   constexpr auto minVal = cuda::std::numeric_limits<I>::min();
   constexpr auto maxVal = cuda::std::numeric_limits<I>::max();
@@ -105,41 +102,41 @@ __host__ __device__ constexpr void test_unsigned(int zero_value)
 
   // Litmit values (0, 1, min, max)
   printf("%d\n", __LINE__);
-  test_add_sat<I>(I{0}, I{0}, I{0}, zero_value);
+  test_add_sat<I>(I{0}, I{0}, I{0});
   printf("%d\n", __LINE__);
-  test_add_sat<I>(I{0}, I{1}, I{1}, zero_value);
+  test_add_sat<I>(I{0}, I{1}, I{1});
   printf("%d\n", __LINE__);
-  test_add_sat<I>(I{0}, minVal, I{0}, zero_value);
+  test_add_sat<I>(I{0}, minVal, I{0});
   printf("%d\n", __LINE__);
-  test_add_sat<I>(I{0}, maxVal, maxVal, zero_value);
+  test_add_sat<I>(I{0}, maxVal, maxVal);
   printf("%d\n", __LINE__);
-  test_add_sat<I>(I{1}, I{0}, I{1}, zero_value);
+  test_add_sat<I>(I{1}, I{0}, I{1});
   printf("%d\n", __LINE__);
-  test_add_sat<I>(I{1}, I{1}, I{2}, zero_value);
+  test_add_sat<I>(I{1}, I{1}, I{2});
   printf("%d\n", __LINE__);
-  test_add_sat<I>(I{1}, minVal, I{1}, zero_value);
+  test_add_sat<I>(I{1}, minVal, I{1});
   printf("%d\n", __LINE__);
-  test_add_sat<I>(I{1}, maxVal, maxVal, zero_value);
+  test_add_sat<I>(I{1}, maxVal, maxVal);
   printf("%d\n", __LINE__); // saturated
-  test_add_sat<I>(minVal, I{0}, I{0}, zero_value);
+  test_add_sat<I>(minVal, I{0}, I{0});
   printf("%d\n", __LINE__);
-  test_add_sat<I>(minVal, I{1}, I{1}, zero_value);
+  test_add_sat<I>(minVal, I{1}, I{1});
   printf("%d\n", __LINE__);
-  test_add_sat<I>(minVal, minVal, minVal, zero_value);
+  test_add_sat<I>(minVal, minVal, minVal);
   printf("%d\n", __LINE__);
-  test_add_sat<I>(minVal, maxVal, maxVal, zero_value);
+  test_add_sat<I>(minVal, maxVal, maxVal);
   printf("%d\n", __LINE__);
-  test_add_sat<I>(maxVal, I{0}, maxVal, zero_value);
-  test_add_sat<I>(maxVal, I{1}, maxVal, zero_value);
+  test_add_sat<I>(maxVal, I{0}, maxVal);
+  test_add_sat<I>(maxVal, I{1}, maxVal);
   printf("%d\n", __LINE__); // saturated
-  test_add_sat<I>(maxVal, minVal, maxVal, zero_value);
+  test_add_sat<I>(maxVal, minVal, maxVal);
   printf("%d\n", __LINE__);
-  test_add_sat<I>(maxVal, maxVal, maxVal, zero_value);
+  test_add_sat<I>(maxVal, maxVal, maxVal);
   printf("%d\n", __LINE__); // saturated
 
   // No saturation (no limit values)
 
-  test_add_sat<I>(I{27}, I{28}, I{55}, zero_value);
+  test_add_sat<I>(I{27}, I{28}, I{55});
 
   // Saturation (no limit values)
 
@@ -147,42 +144,42 @@ __host__ __device__ constexpr void test_unsigned(int zero_value)
     constexpr I x = maxVal / I{2} + I{27};
     constexpr I y = maxVal / I{2} + I{28};
     printf("%d\n", __LINE__); // saturated
-    test_add_sat<I>(x, y, maxVal, zero_value); // saturated
+    test_add_sat<I>(x, y, maxVal); // saturated
     printf("%d\n", __LINE__); // saturated
-    test_add_sat<I>(x, maxVal, maxVal, zero_value); // saturated
+    test_add_sat<I>(x, maxVal, maxVal); // saturated
     printf("%d\n", __LINE__); // saturated
-    test_add_sat<I>(maxVal, y, maxVal, zero_value); // saturated
+    test_add_sat<I>(maxVal, y, maxVal); // saturated
   }
 }
 
-__host__ __device__ constexpr bool test(int zero_value)
+__host__ __device__ constexpr bool test()
 {
-  test_signed<signed char>(zero_value);
-  test_signed<signed short>(zero_value);
-  test_signed<signed int>(zero_value);
-  test_signed<signed long>(zero_value);
-  test_signed<signed long long>(zero_value);
-#if _CCCL_HAS_INT128()
-  test_signed<__int128_t>(zero_value);
-#endif // _CCCL_HAS_INT128()
+  //  test_signed<signed char>(zero_value);
+  //  test_signed<signed short>(zero_value);
+  //  test_signed<signed int>(zero_value);
+  //  test_signed<signed long>(zero_value);
+  //  test_signed<signed long long>(zero_value);
+  // #if _CCCL_HAS_INT128()
+  //  test_signed<__int128_t>(zero_value);
+  // #endif // _CCCL_HAS_INT128()
 
-  test_unsigned<unsigned char>(zero_value);
-  test_unsigned<unsigned short>(zero_value);
-  test_unsigned<unsigned int>(zero_value);
-  test_unsigned<unsigned long>(zero_value);
-  test_unsigned<unsigned long long>(zero_value);
-#if _CCCL_HAS_INT128()
-  test_unsigned<__uint128_t>(zero_value);
-#endif // _CCCL_HAS_INT128()
-
+  //  test_unsigned<unsigned char>(zero_value);
+  test_unsigned<unsigned short>();
+  //  test_unsigned<unsigned int>(zero_value);
+  //  test_unsigned<unsigned long>(zero_value);
+  //  test_unsigned<unsigned long long>(zero_value);
+  // #if _CCCL_HAS_INT128()
+  //  test_unsigned<__uint128_t>(zero_value);
+  // #endif // _CCCL_HAS_INT128()
+  //
   return true;
 }
 
 int main(int, char**)
 {
-  volatile int zero_value = 0;
+  // volatile int zero_value = 0;
 
-  test(zero_value);
+  test();
   // static_assert(test(0));
 
   return 0;
