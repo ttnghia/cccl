a0638e4224d20efc16e8682f517ed323ffe25332
David Bayer <bayer.david99@gmail.com>
2025-06-27 11:26:38 +0200

    fix `cuda_error` for nvrtc

--- libcudacxx/include/cuda/std/__exception/cuda_error.h
+++ libcudacxx/include/cuda/std/__exception/cuda_error.h
@@ -22,26 +22,26 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cuda/std/__exception/exception_macros.h>
-#include <cuda/std/__exception/terminate.h>
-#include <cuda/std/source_location>
-
 #if !_CCCL_COMPILER(NVRTC)
+
+#  include <cuda/std/__exception/exception_macros.h>
+#  include <cuda/std/__exception/terminate.h>
+#  include <cuda/std/source_location>
+
+#  include <nv/target>
+
 #  include <cstdio>
 #  include <stdexcept>
-#endif // !_CCCL_COMPILER(NVRTC)
 
-#include <nv/target>
-
-#include <cuda/std/__cccl/prologue.h>
+#  include <cuda/std/__cccl/prologue.h>
 
 _LIBCUDACXX_BEGIN_NAMESPACE_CUDA
 
-#if _CCCL_HAS_CTK()
+#  if _CCCL_HAS_CTK()
 using __cuda_error_t = ::cudaError_t;
-#else
+#  else
 using __cuda_error_t = int;
-#endif
+#  endif
 
 namespace __detail
 {
@@ -51,7 +51,7 @@ struct __msg_storage
   char __buffer[512]{0};
 };
 
-static char* __format_cuda_error(
+[[nodiscard]] _CCCL_HOST_API inline char* __format_cuda_error(
   __msg_storage& __msg_buffer,
   const __cuda_error_t __status,
   const char* __msg,
@@ -66,11 +66,11 @@ static char* __format_cuda_error(
     __loc.line(),
     __api ? __api : "",
     __api ? " " : "",
-#if _CCCL_HAS_CTK()
+#  if _CCCL_HAS_CTK()
     ::cudaGetErrorString(__status),
-#else // ^^^ _CCCL_HAS_CTK() ^^^ / vvv !_CCCL_HAS_CTK() vvv
+#  else // ^^^ _CCCL_HAS_CTK() ^^^ / vvv !_CCCL_HAS_CTK() vvv
     "cudaError",
-#endif // ^^^ !_CCCL_HAS_CTK() ^^^
+#  endif // ^^^ !_CCCL_HAS_CTK() ^^^
     __status,
     __msg);
   return __msg_buffer.__buffer;
@@ -84,16 +84,17 @@ static char* __format_cuda_error(
 class cuda_error : public ::std::runtime_error
 {
 public:
-  cuda_error(const __cuda_error_t __status,
-             const char* __msg,
-             const char* __api                    = nullptr,
-             _CUDA_VSTD::source_location __loc    = _CUDA_VSTD::source_location::current(),
-             __detail::__msg_storage __msg_buffer = {}) noexcept
+  _CCCL_HOST_API cuda_error(
+    const __cuda_error_t __status,
+    const char* __msg,
+    const char* __api                    = nullptr,
+    _CUDA_VSTD::source_location __loc    = _CUDA_VSTD::source_location::current(),
+    __detail::__msg_storage __msg_buffer = {}) noexcept
       : ::std::runtime_error(__detail::__format_cuda_error(__msg_buffer, __status, __msg, __api, __loc))
       , __status_(__status)
   {}
 
-  [[nodiscard]] auto status() const noexcept -> __cuda_error_t
+  [[nodiscard]] _CCCL_HOST_API auto status() const noexcept -> __cuda_error_t
   {
     return __status_;
   }
@@ -108,14 +109,16 @@ private:
   [[maybe_unused]] const char* __api                 = nullptr,
   [[maybe_unused]] _CUDA_VSTD::source_location __loc = _CUDA_VSTD::source_location::current())
 {
-#if _CCCL_HAS_CTK()
+#  if _CCCL_HAS_CTK()
   NV_IF_TARGET(NV_IS_HOST, (::cudaGetLastError();)) // clear CUDA error state
-#endif // _CCCL_HAS_CTK()
+#  endif // _CCCL_HAS_CTK()
   _CCCL_THROW(::cuda::cuda_error(__status, __msg, __api, __loc));
 }
 
 _LIBCUDACXX_END_NAMESPACE_CUDA
 
-#include <cuda/std/__cccl/epilogue.h>
+#  include <cuda/std/__cccl/epilogue.h>
+
+#endif // !_CCCL_COMPILER(NVRTC)
 
 #endif // _LIBCUDACXX___EXCEPTION_CUDA_ERROR_H
